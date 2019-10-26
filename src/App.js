ae2fb4824c01b52952b121ac1eccf9b312401099
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2019-10-27 00:07:37 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import './css/main.css';
-import Header from './components/Header';
+import React from "react";
+import "./css/main.css";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <div id="body">
+      <div id="bg-img">
+        <div className="overlay"></div>
+        <Dashboard />
+      </div>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,47 @@
-import React from 'react'
+import React from "react";
+
+import '../css/main.css';
 
 function Header() {
-    return (
-        <React.Fragment>
+  return (
+    <React.Fragment>
+      <header>
         <div className="menu-btn">
-            <div className="btn-line"></div>
-            <div className="btn-line"></div>
-            <div className="btn-line"></div>
+          <div className="btn-line"></div>
+          <div className="btn-line"></div>
+          <div className="btn-line"></div>
         </div>
 
         <nav className="menu">
-            <div className="menu-branding">
-                <div className="portrait">
-
-                </div>
-            </div>
-            <ul className="menu-nav">
-                <li className="nav-item">
-                    <a href="" className="nav-link">HOME</a>
-                </li>
-                <li className="nav-item">
-                    <a href="" className="nav-link">ABOUT ME</a>
-                </li>
-                <li className="nav-item">
-                    <a href="" className="nav-link">MY WORK</a>
-                </li>
-                <li className="nav-item">
-                    <a href="" className="nav-link">HOW TO REACH ME</a>
-                </li>
-            </ul>
+          <div className="menu-branding">
+            <div className="portrait"></div>
+          </div>
+          <ul className="menu-nav">
+            <li className="nav-item">
+              <a href="" className="nav-link">
+                HOME
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="" className="nav-link">
+                ABOUT ME
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="" className="nav-link">
+                MY WORK
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="" className="nav-link">
+                HOW TO REACH ME
+              </a>
+            </li>
+          </ul>
         </nav>
-        </React.Fragment>
-    )
+      </header>
+    </React.Fragment>
+  );
 }
 
-export default Header
+export default Header;
