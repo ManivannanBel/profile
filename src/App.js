a36e48083406d1b5e5b17b450e9e823cc507dcd7
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2019-10-26 21:16:59 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css';
 import './css/main.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <React.Fragment>
+        <div className="menu-btn">
+            <div className="btn-line"></div>
+            <div className="btn-line"></div>
+            <div className="btn-line"></div>
+        </div>
+
+        <nav className="menu">
+            <div className="menu-branding">
+                <div className="portrait">
+
+                </div>
+            </div>
+            <ul className="menu-nav">
+                <li className="nav-item">
+                    <a href="" className="nav-link">HOME</a>
+                </li>
+                <li className="nav-item">
+                    <a href="" className="nav-link">ABOUT ME</a>
+                </li>
+                <li className="nav-item">
+                    <a href="" className="nav-link">MY WORK</a>
+                </li>
+                <li className="nav-item">
+                    <a href="" className="nav-link">HOW TO REACH ME</a>
+                </li>
+            </ul>
+        </nav>
+        </React.Fragment>
+    )
+}
+
+export default Header
