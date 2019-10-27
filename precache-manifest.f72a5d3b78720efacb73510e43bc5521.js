af578f86c7b62dc60a102603b734bfae9a03ab6d
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2019-10-27 23:51:34 +0530

    Updates

---
+++ precache-manifest.f72a5d3b78720efacb73510e43bc5521.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f70246924d44f5c6e4ef4b323e333927",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "2fbf55d14c0ab3b88c94",
+    "url": "/profile/static/css/main.e19394af.chunk.css"
+  },
+  {
+    "revision": "32d34ed9006c45286612",
+    "url": "/profile/static/js/2.4a2cc250.chunk.js"
+  },
+  {
+    "revision": "2fbf55d14c0ab3b88c94",
+    "url": "/profile/static/js/main.0aa6be60.chunk.js"
+  },
+  {
+    "revision": "f22687e45198e16003c2",
+    "url": "/profile/static/js/runtime-main.95acc819.js"
+  },
+  {
+    "revision": "fc1ad29c48596a62dc74acf5d6521654",
+    "url": "/profile/static/media/background.fc1ad29c.jpg"
+  },
+  {
+    "revision": "498cf41b50976e3c5506c672b649b2eb",
+    "url": "/profile/static/media/myimage.498cf41b.jpg"
+  }
+]);
